a7601ce4c810d558521d25933de419ada6475dd8
Val <btwonu@gmail.com>
2020-06-07 19:26:43 +0300

    added button animations, navbar buttons animations, experimental JS animation and some advance on the footer

---
+++ scripts/app.js
@@ -0,0 +1,15 @@
+
+(function() {
+  const header = document.querySelector('header');
+  const headerTitle = document.querySelector('h1');
+  const headerBtn = document.querySelector('header button');
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    header.classList.add('fade');
+    headerTitle.classList.add('fade');
+    headerBtn.classList.add('fade')
+  });
+  
+  console.log('hi');
+})();
+
