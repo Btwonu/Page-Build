37e71a80f2b69f52c32a0d330a10c4b3edfa62ac
Val <btwonu@gmail.com>
2020-06-10 13:54:51 +0300

    fixed minor display bug

--- hamburger.js
+++ hamburger.js
@@ -5,6 +5,8 @@ let menuOpened = false;
 
 
 navbarUl.classList.add('hidden');
+navbar.style.height = '4.2rem';
+
 hamburgerButton.addEventListener('click', (e) => {
   if (!menuOpened) {
     hamburgerButton.classList.add('open');
@@ -19,7 +21,7 @@ hamburgerButton.addEventListener('click', (e) => {
   if (menuOpened) {
     navbar.style.height = 'auto';
   } else {
-    navbar.style.height = '4rem';
+    navbar.style.height = '4.2rem';
   }
 
   navbarUl.classList.toggle('hidden');
